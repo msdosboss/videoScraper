4a59170889bc0600cb8f1b50169117efba7c3a9a
orcalord <jacobt0722@gmail.com>
2024-02-25 10:45:18 -0800

    Did stuff with beautiful soup

--- scrape.py
+++ scrape.py
@@ -1,9 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
+r = requests.get('https://www.youtube.com/watch?v=wDchsz8nmbo')
 
 
 print(r)
 
-#print(r.content)
+soup = BeautifulSoup(r.content, 'html5lib')
+
+print(soup.prettify())
+
+#s = soup.find('iframe')
+videoTags = soup.findAll('iframe')
+print("Total", len(videoTags),"videos found")
+print(videoTags)
